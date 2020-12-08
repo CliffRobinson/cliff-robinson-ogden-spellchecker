7b482b91c1f7a58bfe7e9ddd1f04f1bd1fe4e92c
CliffRobinson <clifford.f.robinson@gmail.com>
2020-12-08 20:02:34 +1300

    basic visuals, comment 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+
 import './App.css';
 
 //CFR Comment 1: The app as described has no need to access a backend, 
 //I'd like to have a browser GUI if possible, and React is my framework of choice,
 //so create-react-app is a great place to start. 
 //I like to practise TDD where possible, so let's get Enzyme set up for snapshots next. 
+
+//CFR Comment 3: This is a first pass at how I generally want the app to behave - one
+// area for the user to input text, and another that displays the text with markup to
+// show which words are mispelled. Both inputs are going to be stateless components, with
+// app acting as a container and store for the user input string. For now the next step is 
+// going to be breaking out the user input box into a component.
 function App() {
+
+  const [userInputString, setUserInputString] = useState("initial string")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Ogden's Simplified Spellchecker!</h3>
+      <input type='text' value={userInputString}></input>
+      <input type='text' value={userInputString} disabled={true}></input>
     </div>
   );
 }
