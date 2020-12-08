75032424fbc985efb61ae3bd5841a4e9852641c0
CliffRobinson <clifford.f.robinson@gmail.com>
2020-12-08 21:56:53 +1300

    final comments

--- src/components/OutputBox.test.js
+++ src/components/OutputBox.test.js
@@ -18,3 +18,20 @@ test('userinputbox render', () => {
   const tree = mount(<OutputBox wordArray={quote}/>)
   expect(tree).toMatchSnapshot()
 })
+
+//CFR Comment 9: And with that test written my two hours are up. 
+// There are two main things I would add if I had more time.
+// The first is a bit of basic CSS so that the input and output boxes match each other visually,
+// and a little visual polish such as a nice background. The following website:
+// https://cliffrobinson.github.io/EDA-projects/caesar/caesar.html
+// is _very_ old code but it reflects what I would have preferred to produce visually.
+
+// The second would address the fact that the list given is not really complete and 
+// is not by itself sufficient to produce meaningful english sentences. There are lots of words
+// which are 'not on the list but are there':
+// http://ogden.basic-english.org/wordthere.html
+// and they are needed for meaningful communication. It would be possible to add the words listed
+// on that site in a fairly short amount of time, but the more comprehensive way to do it, 
+// if a person had a day or so, would be to go through every word, and manually list out variations.
+// English words are often irregular, especially the commonly used ones, so it would be a very
+// thing to automate and with only 850 words fairly quick to do manually. 
