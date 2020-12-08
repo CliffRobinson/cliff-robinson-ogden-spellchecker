2ae2df5145392a33368bc1cdc769317ae7eee5dd
CliffRobinson <clifford.f.robinson@gmail.com>
2020-12-08 19:44:27 +1300

    first commit, first comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+//CFR Comment 1: The app as described has no need to access a backend, 
+//I'd like to have a browser GUI if possible, and React is my framework of choice,
+//so create-react-app is a great place to start. 
+//I like to practise TDD where possible, so let's get Enzyme set up for snapshots next. 
 function App() {
   return (
     <div className="App">
