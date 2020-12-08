be46d77f9407ad8b98d0674c3014473578094ddc
CliffRobinson <clifford.f.robinson@gmail.com>
2020-12-08 21:26:13 +1300

    parse sentences, comment 8

--- src/checkerFunctions.js
+++ src/checkerFunctions.js
@@ -3,3 +3,16 @@ import {dictionaryObj} from  './dictionary.js'
 export function checkWord(word) {
     return dictionaryObj[word];
 }
+
+export function checkSentence(inputString) {
+    return inputString
+    .split(" ")
+    .map(word => ({
+        word: word,
+        spelling: checkWord(depunctuate(word.toLowerCase()))
+    }))
+}
+
+export function depunctuate(word) {
+    return word.replace(/[^\w\s]|_/g, "")
+}
--- src/checkerFunctions.test.js
+++ src/checkerFunctions.test.js
@@ -1,4 +1,4 @@
-import {checkWord} from './checkerFunctions'
+import {checkWord, checkSentence, depunctuate} from './checkerFunctions'
 
 test('checkWord', () => {
     expect(checkWord("late")).toBe(true)
@@ -7,3 +7,30 @@ test('checkWord', () => {
 
 //CFR Comment 7: We now have a way of verifying if individual words are in Ogden's dictionary.
 //Now we sort out whole strings. 
+
+test('checkSentence', () => {
+    const sentence = "A wizard is never late, nor is he early,"
+
+    const expected = [
+        { word: 'A', spelling: true },
+        { word: 'wizard', spelling: undefined },
+        { word: 'is', spelling: undefined },
+        { word: 'never', spelling: undefined },
+        { word: 'late,', spelling: true },
+        { word: 'nor', spelling: undefined },
+        { word: 'is', spelling: undefined },
+        { word: 'he', spelling: true },
+        { word: 'early,', spelling: true }
+      ]
+  
+    const actual = checkSentence(sentence)
+    expect(actual.toString()).toEqual(expected.toString())
+})
+
+test('depunctuate', () => {
+    expect(depunctuate("a@+-nimal!")).toEqual("animal")
+    expect(depunctuate("human")).toEqual("human")
+})
+
+//CFR Comment 8: we now have a way of parseing our input string that checks all spellings but preserves
+// any punctuation the user has put in. 
